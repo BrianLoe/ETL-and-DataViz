eeb84669f518b0f96510ec6535d24b3a0d520513
BrianLoe <corneliusbrian.loe@students.mq.edu.au>
2023-06-20 22:19:30 +1000

    add one query

--- human_resource/HR/sql_query.sql
+++ human_resource/HR/sql_query.sql
@@ -1,17 +1,24 @@
-select j."Department", avg("Salary"), count("EmployeeID") as "No. of Employees"
+select j.department, round(avg("salary(usd)"),2), count(employeeid) as "No. of Employees"
 from main_details m left join
-job_details j on m."Job_Profile"=j."Job_Profile"
+job_details j on m.job_profile=j.job_profile
 group by 1 
 order by 2;
 
-select j."level", count("EmployeeID") as "No. of Employees"
+select j.level, count(employeeid) as "No. of Employees"
 from main_details m left join
-job_details j on m."Job_Profile"=j."Job_Profile"
+job_details j on m.job_profile=j.job_profile
 group by 1 
 order by 2;
 
-select o."office_name", o."office_type", count("EmployeeID") as "No. of Employees"
+select o.office_name, o.office_type, count(employeeid) as "No. of Employees"
 from main_details m left join
-office o on m."office_id"=o."office_id"
+office o on m.office_id=o.office_id
 group by cube(1,2)
 order by 1,3 desc;
+
+select j.job_title, max("salary(usd)") as "Highest Salary"
+from main_details m left join
+job_details j on m.job_profile=j.job_profile
+group by 1 
+order by 2 desc
+limit 10;
